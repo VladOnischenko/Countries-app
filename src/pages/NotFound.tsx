709c231563e60e7c5fa91d1dac02fc44b8eb1132
Vlad <vladonish@gmail.com>
2022-09-10 18:35:39 +0300

    add styles for NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+import {Button} from "../components/Button/Button";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    margin-top: 2rem;
+    font-weight: var(--fw-bold);
+`
 
 export const NotFound = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            NotFound
-        </div>
+        <section>
+            <Button onClick={() => navigate("/", {replace: true})}>
+                Go Home
+            </Button>
+            <Wrapper>
+                Page not found, please go to the Home page!
+            </Wrapper>
+        </section>
     );
 };
